c302bb0b90255b1f708ddd02dda4f28ba4d75036
Alekmz <62714545+Alekmz@users.noreply.github.com>
2025-05-05 20:01:17 -0300

    pair programming exercises - nested while loops

---
+++ senha.js
@@ -0,0 +1,16 @@
+let senha = "Alex123"
+let tentativas = 1
+let acertou = false
+while(tentativas <= 3 && acertou === false){
+    let senhatentativa = prompt("Digite sua senha: ")
+    if(senha === senhatentativa){
+        alert("Senha correta, usuário logado!")
+        acertou = true
+    } else{
+        let tentativassobrando = 3 - tentativas
+        alert("Senha incorreta! Você tem mais: " + tentativassobrando + " Tentativas" )
+    }
+    tentativas++
+}
+if(acertou === false) alert("Conta bloqueada!")
+else alert("Você está dentro do sistema!")
